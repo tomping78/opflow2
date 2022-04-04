46289ff900b364a31711f0e245386c22fb99abf2
Ninezero <ninezero90hy@gmail.com>
2022-04-04 13:36:36 +0900

    [스토리북] 타이틀 수정

--- src/stories/Page.stories.tsx
+++ src/stories/Page.stories.tsx
@@ -5,7 +5,7 @@ import { Page } from './Page';
 import Boards from '../components/pages/Boards';
 
 export default {
-  title: '예제/페이지', // 스토리북에서 보여질 그룹과 경로 표시
+  title: '예제/페이지/목록', // 스토리북에서 보여질 그룹과 경로 표시
   component: Boards, // 어떤 컴포넌트를 문서화 할지
   parameters: {
     // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
@@ -15,10 +15,10 @@ export default {
 
 const Template: ComponentStory<typeof Boards> = args => <Boards />;
 
-export const 게시판_목록 = Template.bind({});
+export const 게시판 = Template.bind({});
 
 // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
-게시판_목록.play = async ({ canvasElement }) => {
+게시판.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement);
   const loginButton = await canvas.getByRole('button', { name: /Log in/i });
   await userEvent.click(loginButton);
