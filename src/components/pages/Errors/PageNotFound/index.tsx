fecb44bf5590842aee41c2d94d20664919a5b612
Ninezero <ninezero90hy@gmail.com>
2022-04-04 17:19:01 +0900

    [Pages/PageNotFound] 404 오류 페이지 수정
     - 홈으로 이동 버튼 추가 및 기본 오류 페이지 스타일 적용

--- src/components/pages/Errors/PageNotFound/index.tsx
+++ src/components/pages/Errors/PageNotFound/index.tsx
@@ -1,4 +1,6 @@
-import BaseTemplate from '../../../templates/BaseTemplate';
+import BaseTemplate from "../../../templates/BaseTemplate";
+import { Button, Result } from "antd";
+import { useNavigate } from "react-router-dom";
 
 /**
  * 페이지를 찾지 못했습니다
@@ -6,9 +8,26 @@ import BaseTemplate from '../../../templates/BaseTemplate';
  * @constructor
  */
 const PageNotFound = () => {
+  const navigate = useNavigate();
+
   return (
     <BaseTemplate>
-      <div className={'404'}>404</div>
+      <Result
+        style={{
+          position: 'relative',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+        }}
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <Button type="primary" onClick={() => navigate('/')}>
+            홈으로 이동
+          </Button>
+        }
+      />
     </BaseTemplate>
   );
 };
