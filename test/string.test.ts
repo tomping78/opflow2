86cebaa6c841bca6da7803f7eadb5c002d97ee40
Ninezero <ninezero90hy@gmail.com>
2022-04-05 17:58:58 +0900

    [#9] 콤마 처리 함수 추가

---
+++ test/string.test.ts
@@ -0,0 +1,16 @@
+import { addComma } from "../src/common/utils/string-utils";
+
+describe("[string-utils:콤마_추가]", () => {
+  it("100", () => expect(addComma(100)).toEqual("100"));
+  it("1000", () => expect(addComma(1000)).toEqual("1,000"));
+  it("10000", () => expect(addComma(10000)).toEqual("10,000"));
+  it("100000", () => expect(addComma(100000)).toEqual("100,000"));
+  it("1000000", () => expect(addComma(1000000)).toEqual("1,000,000"));
+  it("10000000", () => expect(addComma(10000000)).toEqual("10,000,000"));
+  it("100000000", () => expect(addComma(100000000)).toEqual("100,000,000"));
+  it("1000000000", () => expect(addComma(1000000000)).toEqual("1,000,000,000"));
+  it("10000000000", () => expect(addComma(10000000000)).toEqual("10,000,000,000"));
+  it("100000000000", () => expect(addComma(100000000000)).toEqual("100,000,000,000"));
+  it("1000000000000", () => expect(addComma(1000000000000)).toEqual("1,000,000,000,000"));
+  it("10000000000000", () => expect(addComma(10000000000000)).toEqual("10,000,000,000,000"));
+});
