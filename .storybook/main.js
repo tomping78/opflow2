8ea3e2889a454ba5825e6be245cd91e0b52eb02e
circlegiven <circlegiven@gmail.com>
2022-04-06 16:20:29 +0900

    [스토리북] control 애드온 제거 및 addon-docs 상위로 이동

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,6 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@storybook/addon-controls',
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/preset-create-react-app',
     {
       name: '@storybook/addon-docs',
       options: {
@@ -13,6 +9,9 @@ module.exports = {
         },
       },
     },
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
   ],
   framework: '@storybook/react',
   core: {
