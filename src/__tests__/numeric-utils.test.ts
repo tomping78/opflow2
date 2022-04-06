ad40a8708c1389399a3cbc9ee14b83646386029a
Ninezero <ninezero90hy@gmail.com>
2022-04-06 14:27:42 +0900

    [#9] 콤마 처리 함수 테스트 코드 수정

--- src/__tests__/numeric-utils.test.ts
+++ src/__tests__/numeric-utils.test.ts
@@ -1,3 +1,12 @@
-describe('[numeric-utils:콤마_추가]', () => {
-  it('true', () => expect(1).toEqual(1));
+import { addComma } from "../common/utils/numeric-utils";
+
+describe("[numeric-utils:콤마_추가]", () => {
+  it("100", () => expect(addComma(100)).toEqual("100"));
+  it("1000", () => expect(addComma(1000)).toEqual("1,000"));
+  it("10000", () => expect(addComma(10000)).toEqual("10,000"));
+  it("100000", () => expect(addComma(100000)).toEqual("100,000"));
+  it("1000000", () => expect(addComma(1000000)).toEqual("1,000,000"));
+  it("10000000", () => expect(addComma(10000000)).toEqual("10,000,000"));
+  it("100000000", () => expect(addComma(100000000)).toEqual("100,000,000"));
+  it("1000000000", () => expect(addComma(1000000000)).toEqual("1,000,000,000"));
 });
