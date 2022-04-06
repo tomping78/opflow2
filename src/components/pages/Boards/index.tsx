efe187fb962e2c216306b63d8bb11acc9ed8d73c
circlegiven <circlegiven@gmail.com>
2022-04-06 09:53:52 +0900

    [공통] 페이지 filter fragment 태그 제거

--- src/components/pages/Boards/index.tsx
+++ src/components/pages/Boards/index.tsx
@@ -52,30 +52,24 @@ const filters: FilterProp[] = [
     name: 'category',
     label: '카테고리',
     component: (
-      <>
-        <Select
-          defaultValue="lucy"
-          placeholder={'선택해주세요'}
-          style={{ width: 250 }}
-        >
-          <Select.Option value="jack">Jack</Select.Option>
-          <Select.Option value="lucy">Lucy</Select.Option>
-        </Select>
-        &nbsp; &nbsp;
-      </>
+      <Select
+        defaultValue="lucy"
+        placeholder={'선택해주세요'}
+        style={{ width: 250 }}
+      >
+        <Select.Option value="jack">Jack</Select.Option>
+        <Select.Option value="lucy">Lucy</Select.Option>
+      </Select>
     ),
   },
   {
     name: 'country',
     label: '국가',
     component: (
-      <>
-        <Select style={{ width: 250 }} placeholder={'선택해주세요'}>
-          <Select.Option value="KOR">KOR</Select.Option>
-          <Select.Option value="JPN">JPN</Select.Option>
-        </Select>
-        &nbsp; &nbsp;
-      </>
+      <Select style={{ width: 250 }} placeholder={'선택해주세요'}>
+        <Select.Option value="KOR">KOR</Select.Option>
+        <Select.Option value="JPN">JPN</Select.Option>
+      </Select>
     ),
   },
   {
