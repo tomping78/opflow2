b14aa8048804ebbff1e00ba8905f2ce7ab1ace80
Ninezero <ninezero90hy@gmail.com>
2022-04-11 14:23:14 +0900

    [공지사항] 즉시 검색 예제 추가

--- src/components/pages/Notices/index.tsx
+++ src/components/pages/Notices/index.tsx
@@ -1,5 +1,79 @@
 import React from 'react';
-import { DefaultTemplate as ListTemplate } from '../../templates/ListTemplate';
+import { ListTemplateA } from '../../templates/ListTemplate';
+import { format } from 'date-fns';
+import { DeleteOutlined } from '@ant-design/icons';
+import { message, Select } from 'antd';
+import { FilterProp } from '../../molecules/Filter';
+import Search from '../../atoms/Search';
+
+const columns = [
+  {
+    title: '제목',
+    dataIndex: 'title',
+    sorter: true,
+    render: (title: string) => `${title}`,
+    width: '20%',
+  },
+  {
+    title: '내용',
+    dataIndex: 'content',
+  },
+  {
+    title: '작성자',
+    dataIndex: 'author',
+    width: '10%',
+  },
+  {
+    title: '수정자',
+    dataIndex: 'XXXXX',
+    width: '10%',
+    render: (XXXXX: string) => {
+      return <span style={{ color: 'red' }}>{XXXXX ?? 'NULL'}</span>;
+    },
+  },
+  {
+    title: '생성일',
+    dataIndex: 'createdDate',
+    width: '15%',
+    render: (createdDate: Date) => format(new Date(createdDate), 'yyyy-MM-dd'),
+  },
+  {
+    title: '',
+    dataIndex: 'id',
+    width: '3%',
+    render: () => (
+      <DeleteOutlined onClick={() => message.info('삭제 되었습니다.')} />
+    ),
+  },
+];
+
+const filters: FilterProp[] = [
+  {
+    name: 'category',
+    label: '카테고리',
+    component: (
+      <Select placeholder={'선택해주세요'}>
+        <Select.Option value="jack">Jack</Select.Option>
+        <Select.Option value="lucy">Lucy</Select.Option>
+      </Select>
+    ),
+  },
+  {
+    name: 'country',
+    label: '국가',
+    component: (
+      <Select placeholder={'선택해주세요'}>
+        <Select.Option value="KOR">KOR</Select.Option>
+        <Select.Option value="JPN">JPN</Select.Option>
+      </Select>
+    ),
+  },
+  {
+    name: 'keyword',
+    label: '검색어',
+    component: <Search placeholder={'제목이나 내용을 입력하세요'} />,
+  },
+];
 
 /**
  * Notices Component
@@ -31,11 +105,14 @@ const Notices = () => {
    ******************************************/
 
   return (
-    <>
-      <ListTemplate>
-        <span>여기서 하시면 됩니다</span>
-      </ListTemplate>
-    </>
+    <ListTemplateA
+      useImmediatelySearch={true}
+      title={'공지사항'}
+      subTitle="공지사항"
+      url={'/api/boards'}
+      columns={columns}
+      filters={filters}
+    />
   );
 };
 
