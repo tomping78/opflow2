c7e9d7856b77976065a082d8b9ea47ca7655ba98
Ninezero <ninezero90hy@gmail.com>
2022-04-11 10:27:42 +0900

    [numeric-utils] 값이 Null 이거나 Undefined 인 경우 0을 반환하도록 처리

--- src/common/utils/numeric-utils.ts
+++ src/common/utils/numeric-utils.ts
@@ -4,4 +4,7 @@ const commaNumber = require('comma-number');
  * 콤마 추가
  * @param num
  */
-export const addComma = (num: number) => commaNumber(num);
+export const addComma = (num: number | null | undefined) => {
+  if (!num) return 0;
+  return commaNumber(num);
+};
