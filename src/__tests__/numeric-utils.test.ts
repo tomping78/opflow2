2495115eaab71a3c32f87f45a07a26bf56e7bc64
ninezero90hy <ninezero90hy@gmail.com>
2022-05-13 16:05:20 +0900

    [numeric-utils] 값이 Null 이거나 Undefined 인 경우에도 반환 타입은 문자열로 반환되도록 변경

--- src/__tests__/numeric-utils.test.ts
+++ src/__tests__/numeric-utils.test.ts
@@ -1,8 +1,8 @@
 import {addComma} from "../common/utils/numeric-utils";
 
 describe("[numeric-utils:콤마_추가]", () => {
-    it("NULL is returned 0", () => expect(addComma(null)).toEqual(0));
-    it("Undefined is returned 0", () => expect(addComma(undefined)).toEqual(0));
+    it("NULL is returned 0", () => expect(addComma(null)).toEqual("0"));
+    it("Undefined is returned 0", () => expect(addComma(undefined)).toEqual("0"));
     it("100", () => expect(addComma(100)).toEqual("100"));
     it("1000", () => expect(addComma(1000)).toEqual("1,000"));
     it("10000", () => expect(addComma(10000)).toEqual("10,000"));
--- src/common/utils/numeric-utils.ts
+++ src/common/utils/numeric-utils.ts
@@ -4,7 +4,7 @@ const commaNumber = require('comma-number');
  * 콤마 추가
  * @param num
  */
-export const addComma = (num: number | null | undefined) => {
-  if (!num) return 0;
+export const addComma = (num: number | null | undefined): string => {
+  if (!num) return "0";
   return commaNumber(num);
 };
